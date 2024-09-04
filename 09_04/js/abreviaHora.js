a320fe0e36197eb260f96aa478cb277b65a5e844
Samara Ribeiro <samara.s.ribeiro@aluno.senai.br>
2024-09-04 17:23:16 -0300

    tabelas

---
+++ 09_04/js/abreviaHora.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function abreviaHora(dataHora){
+
+    const data = new Date(dataHora);
+    const hora = data.toLocaleTimeString('pt-br',{hour:'2-digit', minute:'2-digit'});
+    return hora;
+
+}
+
+export default abreviaHora;
